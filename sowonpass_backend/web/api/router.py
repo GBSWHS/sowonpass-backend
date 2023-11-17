9121f1bdc6c3b4dc844193b9c58691952ecaba54
siwonKH <kanghyunkim313@gmail.com>
2023-11-17 13:10:26 +0900

    init

--- sowonpass_backend/web/api/router.py
+++ sowonpass_backend/web/api/router.py
@@ -1,9 +1,8 @@
 from fastapi.routing import APIRouter
 
-from sowonpass_backend.web.api import docs, dummy, echo, monitoring
+from sowonpass_backend.web.api import docs, echo, monitoring
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
