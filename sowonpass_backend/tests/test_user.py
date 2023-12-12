053af8bb6dd4ea671cd826dc6b30d0fa2844eaa8
siwonKH <kanghyunkim313@gmail.com>
2023-12-12 12:27:24 +0900

    add user tests

---
+++ sowonpass_backend/tests/test_user.py
@@ -0,0 +1,31 @@
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from sowonpass_backend.db.dao.user_dao import UserDAO
+from sowonpass_backend.web.api.user.schema import UserModelInputDTO
+
+
+@pytest.mark.anyio
+async def test_create_read_delete_user(dbsession: AsyncSession) -> None:
+    user_dao = UserDAO(dbsession)
+
+    new_user = UserModelInputDTO(
+        role=1,
+        name="test_name",
+        phone_number="01012345678",
+    )
+
+    await user_dao.create_user(
+        role=new_user.role,
+        name=new_user.name,
+        phone_number=new_user.phone_number,
+    )
+    user = await user_dao.read_user(user_id=1)
+    assert user is not None
+    assert user.role == new_user.role
+    assert user.name == new_user.name
+    assert user.phone_number == new_user.phone_number
+
+    await user_dao.delete_user(user_id=1)
+    user = await user_dao.read_user(user_id=1)
+    assert user is None
