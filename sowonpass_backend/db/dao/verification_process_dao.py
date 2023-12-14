62b6ee74c31979526cdbfab098d7b1f1a25a69f4
siwonKH <kanghyunkim313@gmail.com>
2023-12-14 12:08:43 +0900

    add user to process dao

--- sowonpass_backend/db/dao/verification_process_dao.py
+++ sowonpass_backend/db/dao/verification_process_dao.py
@@ -3,6 +3,7 @@ from sqlalchemy import delete, insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from sowonpass_backend.db.dependencies import get_db_session
+from sowonpass_backend.db.models.process_user import process_user
 from sowonpass_backend.db.models.verification_process import VerificationProcessModel
 
 
@@ -31,5 +32,12 @@ class VerificationProcessDAO:
     async def delete_process(self, process_id: int) -> None:
         stmt = delete(VerificationProcessModel).where(
             VerificationProcessModel.id == process_id,
+        )
+        await self.session.execute(stmt)
+
+    async def add_user_to_process(self, user_id: int, process_id: int) -> None:
+        stmt = insert(process_user).values(
+            verification_process=process_id,
+            user=user_id,
         )
         await self.session.execute(stmt)
