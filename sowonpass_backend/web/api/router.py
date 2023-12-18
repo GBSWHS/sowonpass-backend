9bea1846bf950d7bb5c3e9a092506e29d8483601
siwonKH <kanghyunkim313@gmail.com>
2023-12-18 11:26:56 +0900

    remove echo router

--- sowonpass_backend/web/api/router.py
+++ sowonpass_backend/web/api/router.py
@@ -1,12 +1,11 @@
 from fastapi.routing import APIRouter
 
-from sowonpass_backend.web.api import auth, docs, echo, monitoring, user
+from sowonpass_backend.web.api import auth, docs, monitoring, user
 from sowonpass_backend.web.api import verification_process as process
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(process.router, prefix="/process", tags=["process"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
